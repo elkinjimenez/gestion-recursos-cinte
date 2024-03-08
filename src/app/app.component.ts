103288b54c595871842346b986219da0e5e137c9
elkinjimenez <elkin.jiga@hotmail.com>
2024-03-08 16:53:53 -0500

    Consumiendo servicio en segundo plano para darle un buen inicio al juego.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,24 +11,70 @@ import { WordService } from './services/word/word.service';
 })
 export class AppComponent {
 
+  wordListing = [
+    'gatos',
+    'perros',
+    'ladrillo',
+    'iglesia',
+    'colorido',
+    'árboles',
+    'lapicero',
+    'reloj',
+    'ciudades',
+    'general',
+    'clausura'
+  ]
+
+  isDisabled: boolean[] = [];
+
   orderedWord: string[] = [];
 
   desorderedWord: string[] = [];
 
+  validateWord: string[] = [];
+
   constructor(
     private wordService: WordService,
-  ) { }
+  ) {
+    this.randomWord();
+  }
 
-  ngOnInit() {
-    this.wordService.getWord().subscribe((data) => {
-      if (data) {
-        this.orderedWord = data[0].split('');
-        this.desorderedWord = [...this.orderedWord];
-        this.desorderedWord.sort(() => Math.random() - 0.5);
+  ngOnInit() { }
+
+  ngAfterViewInit() {
+    this.getWordsApi();
+  }
+
+  public randomWord() {
+    this.isDisabled = [];
+    this.validateWord = [];
+    this.wordListing = this.wordListing.sort(() => Math.random() - 0.5);
+    this.orderedWord = this.wordListing[0].split('');
+    this.wordListing.shift();
+    this.desorderedWord = [...this.orderedWord];
+    this.desorderedWord.sort(() => Math.random() - 0.5);
+    if (this.wordListing.length < 5)
+      this.getWordsApi();
+
+  }
+
+  private getWordsApi() {
+    this.wordService.getWord().subscribe((data: string[]) => {
+      if (data && data.length > 0) {
+        data.forEach(word => {
+          if (/^[^\s.!#]{4,8}$/.test(word)) {
+            this.wordListing.push(word);
+          }
+        })
       }
     }, error => {
       alert('Error al consumir el servicio de consulta de palabras: ' + error);
     });
+  }
+
+  public addLetter(letter: string, index: number) {
+    this.validateWord.push(letter);
+    this.isDisabled[index] = true;
   }
 
 }
--- src/app/services/word/word.service.ts
+++ src/app/services/word/word.service.ts
@@ -6,13 +6,13 @@ import { Injectable } from '@angular/core';
 })
 export class WordService {
 
-  private URL = "https://random-word-api.herokuapp.com/word?lang=es";
+  private URL = "https://random-word-api.herokuapp.com/word?lang=es&number=10";
 
   constructor(
     private http: HttpClient
   ) { }
 
   getWord() {
-    return this.http.get<string>(this.URL);
+    return this.http.get<string[]>(this.URL);
   }
 }
